0224a4690ea1ebd908dc51c1c49df9bb5270c89d
Michael Sekol <mjsekol@live.com>
2020-09-03 09:51:35 -0400

    Ninja Commit

--- GitHubTest/Program.cs
+++ GitHubTest/Program.cs
@@ -7,6 +7,11 @@ namespace GitHubTest
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Ninja");
+            Console.WriteLine("Hello Ninja");
+            Console.WriteLine("Hello Ninja");
+            Console.WriteLine("Hello Ninja");
+            Console.WriteLine("Hello Ninja");
         }
     }
 }
